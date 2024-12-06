c94a24dfabc6feae04c20722c79cbe1b66570775
Sariiyy <Sararaji0000@gmail.com>
2024-12-06 21:30:23 +0100

    -#𝗔𝗡𝗗𝗘𝗥𝗟𝗜𝗡𝗔💀

--- index.js
+++ index.js
@@ -47,8 +47,7 @@ const { File } = require('megajs')
 const prefix = '.'
 
 
-const ownerNumber = ['🧚‍♂️💕𝗔𝗱𝗱 𝗬𝗼𝘂𝗿 𝗕𝗼𝘁 𝗡𝘂𝗺𝗯𝗲𝗿 𝗛𝗲𝗿𝗲 𝗪𝗶𝘁𝗵 𝗖𝗼𝘂𝗻𝘁𝗿𝘆 𝗖𝗼𝗱𝗲💕🧚‍♂️']
-
+const ownerNumber = ['+212681-704268'] 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
 if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
